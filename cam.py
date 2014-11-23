f24b06f296a32a2e16d97dc016d789f730cc5d67
gracz21 <kamil.walek21@gmail.com>
2014-11-23 19:37:43 +0100

    Stosunek deskryptorów+rozpoznawanie z kamery

--- cam.py
+++ cam.py
@@ -1,8 +1,10 @@
 from skimage import data, filter, morphology
-from skimage.feature import (match_descriptors, ORB)
+from skimage.feature import (match_descriptors, ORB, plot_matches)
 import os
 from multiprocessing import Pool
 import logging
+import cv2
+import matplotlib.pyplot as plt
 
 types = ['as', 'dwojka', 'trojka', 'czworka',
              'piatka', 'szostka', 'siodemka', 'osemka',
@@ -11,29 +13,36 @@ colors = ['pik', 'kier', 'trefl', 'karo']
 cards = ['joker']
 descriptor_extractor = ORB()
 
+
 def load_pattenrs(filename):
     z_patterns = []
     print 'Working on: ' + filename
-    tmp = data.imread('patterns/' + filename, as_grey=True)
+    img = data.imread('patterns/' + filename, as_grey=True)
+    tmp = img
     tmp = filter.canny(tmp, sigma=3.0)
     tmp = morphology.dilation(tmp, morphology.disk(2))
     descriptor_extractor.detect_and_extract(tmp)
     obj_desc = descriptor_extractor.descriptors
-    z_patterns.append([obj_desc, filename])
+    z_patterns.append([obj_desc, filename, img])
     return z_patterns
 
 
-def load_scenes(filename):
-    zipped_scenes = []
-    print 'Working on: ' + filename
-    tmp = data.imread('scenes/' + filename, as_grey=True)
+def take_pic():
+    camera = cv2.VideoCapture(0)
+    for i in xrange(30):
+        temp, tmp = camera.read()
+    print("Taking image...")
+    retval, img = camera.read()
+    cv2.imwrite("tmp.jpg", img)
+    img = data.imread('tmp.jpg', as_grey=True)
+    tmp = img
     tmp **= 2.0
     tmp = filter.canny(tmp, sigma=3.0)
     tmp = morphology.dilation(tmp, morphology.disk(2))
     descriptor_extractor.detect_and_extract(tmp)
-    scen_desc = descriptor_extractor.descriptors
-    zipped_scenes.append([scen_desc, filename])
-    return zipped_scenes
+    del(camera)
+    return descriptor_extractor.descriptors, img
+
 
 
 def set_names(size):
@@ -47,8 +56,9 @@ def set_names(size):
 def recognize(pattern, name, scene):
     zipped_matches = []
     match = match_descriptors(scene, pattern, cross_check=True)
-    zipped_matches.append([match.size, name])
+    zipped_matches.append([match.size, name, match])
     return zipped_matches
+
 
 def f_wrap(arg_list):
     try:
@@ -61,23 +71,33 @@ def main():
     listing = os.listdir('patterns')
     zipped_patterns = p.map(load_pattenrs, listing)
     zipped_patterns = [ent for sublist in zipped_patterns for ent in sublist]
-    listing = os.listdir('scenes')
-    zipped_scenes = p.map(load_scenes, listing)
-    zipped_scenes = [ent for sublist in zipped_scenes for ent in sublist]
+    print "Get ready..."
     #zipped_patterns.sort(key=lambda x: x[1])
-    #patterns, name = zip(*zipped_patterns)
-    zipped_scenes.sort(key=lambda x: x[1])
-    scenes, tmp = zip(*zipped_scenes)
+    patterns, tmp, img = zip(*zipped_patterns)
     set_names(len(zipped_patterns) - 1)
-    for j in scenes:
-        arg_list = []
-        for a, b in zipped_patterns:
-            arg_list.append([a, b, j])
-        zipped_matches = p.map(f_wrap, arg_list)
-        zipped_matches = [ent for sublist in zipped_matches for ent in sublist]
-        zipped_matches.sort(key=lambda x: x[1])
-        matches, tmp = zip(*zipped_matches)
-        print 'Karta to: ', cards[matches.index(max(matches))]
+    scene_desc, scene = take_pic()
+    arg_list = []
+    for a, b, c in zipped_patterns:
+        arg_list.append([a, b, scene_desc])
+    zipped_matches = p.map(f_wrap, arg_list)
+    zipped_matches = [ent for sublist in zipped_matches for ent in sublist]
+    zipped_matches.sort(key=lambda x: x[1])
+    matches, tmp, match_array = zip(*zipped_matches)
+    best_match = max(matches)
+    proc = 1.0
+    id = 0
+    for i in range(len(patterns)):
+        el = abs(1 - (patterns[i].size/scene_desc.size))
+        if matches[i] == best_match and el < proc:
+            proc = el
+            id = i
+    print 'Karta to: ', cards[id]
+    fig, ax = plt.subplots()
+    plt.gray()
+    plot_matches(ax, img[id], scene, patterns[id], scene_desc, match_array[id])
+    ax.axis('off')
+    plt.show()
+
 
 if __name__ == '__main__':
     main()
---
+++ image.py
@@ -0,0 +1,91 @@
+from skimage import data, filter, morphology
+from skimage.feature import (match_descriptors, ORB)
+import os
+from multiprocessing import Pool
+import logging
+
+types = ['as', 'dwojka', 'trojka', 'czworka',
+             'piatka', 'szostka', 'siodemka', 'osemka',
+             'dziewiatka', 'dziesiatka', 'walet', 'dama', 'krol']
+colors = ['pik', 'kier', 'trefl', 'karo']
+cards = ['joker']
+descriptor_extractor = ORB()
+
+def load_pattenrs(filename):
+    z_patterns = []
+    print 'Working on: ' + filename
+    tmp = data.imread('patterns/' + filename, as_grey=True)
+    tmp = filter.canny(tmp, sigma=3.0)
+    tmp = morphology.dilation(tmp, morphology.disk(2))
+    descriptor_extractor.detect_and_extract(tmp)
+    obj_desc = descriptor_extractor.descriptors
+    z_patterns.append([obj_desc, filename])
+    return z_patterns
+
+
+def load_scenes(filename):
+    zipped_scenes = []
+    print 'Working on: ' + filename
+    tmp = data.imread('scenes/' + filename, as_grey=True)
+    tmp **= 2.0
+    tmp = filter.canny(tmp, sigma=3.0)
+    tmp = morphology.dilation(tmp, morphology.disk(2))
+    descriptor_extractor.detect_and_extract(tmp)
+    scen_desc = descriptor_extractor.descriptors
+    zipped_scenes.append([scen_desc, filename])
+    return zipped_scenes
+
+
+def set_names(size):
+    i = -1
+    while i < size:
+        if i > -1:
+            cards.append(types[i % 13] + ' ' + colors[int(i / 13)])
+        i += 1
+
+
+def recognize(pattern, name, scene):
+    zipped_matches = []
+    match = match_descriptors(scene, pattern, cross_check=True)
+    zipped_matches.append([match.size, name])
+    return zipped_matches
+
+def f_wrap(arg_list):
+    try:
+        return recognize(*arg_list)
+    except Exception:
+        logging.exception("f(%r) failed" % (arg_list,))
+
+def main():
+    p = Pool(5)
+    listing = os.listdir('patterns')
+    zipped_patterns = p.map(load_pattenrs, listing)
+    zipped_patterns = [ent for sublist in zipped_patterns for ent in sublist]
+    listing = os.listdir('scenes')
+    zipped_scenes = p.map(load_scenes, listing)
+    zipped_scenes = [ent for sublist in zipped_scenes for ent in sublist]
+    #zipped_patterns.sort(key=lambda x: x[1])
+    patterns, tmp = zip(*zipped_patterns)
+    zipped_scenes.sort(key=lambda x: x[1])
+    scenes, tmp = zip(*zipped_scenes)
+    set_names(len(zipped_patterns) - 1)
+    for j in scenes:
+        arg_list = []
+        for a, b in zipped_patterns:
+            arg_list.append([a, b, j])
+        zipped_matches = p.map(f_wrap, arg_list)
+        zipped_matches = [ent for sublist in zipped_matches for ent in sublist]
+        zipped_matches.sort(key=lambda x: x[1])
+        matches, tmp = zip(*zipped_matches)
+        best_match = max(matches)
+        proc = 1.0
+        id = 0
+        for i in range(len(patterns)):
+            el = abs((patterns[i].size/j.size) - 1)
+            if matches[i] == best_match and el < proc:
+                proc = el
+                id = i
+        print 'Karta to: ', cards[id]
+
+if __name__ == '__main__':
+    main()

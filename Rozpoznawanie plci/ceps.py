7cecfadec684609de928b68f30bac602cf95f962
gracz21 <kamil.walek21@gmail.com>
2015-01-08 16:34:27 +0100

    Update

--- Rozpoznawanie plci/ceps.py
+++ Rozpoznawanie plci/ceps.py
@@ -7,84 +7,78 @@ import scipy.io.wavfile
 import math
 import os
 import wave
+import struct
 
-#opts, file = getopt.getopt(argv, [])
+def readwav(fname):
+    wav = wave.open(fname, "r")
+    nchannels, sampwidth, framerate, nframes, comptype, compname = wav.getparams()
+    frames = wav.readframes(nframes * nchannels)
+    out = struct.unpack_from("%dh" % nframes * nchannels, frames)
+    if nchannels == 2:
+        left = array(out[0::2], dtype='float_')
+        left /= 2
+        right = array(out[1::2], dtype='float_')
+        right /= 2
+        signal = left + right
+    else:
+        signal = array(out, dtype='float_')
+    w = framerate
+    return w, signal
+
 listing = os.listdir('samples')
 #file = '006_K.wav'
 for file in listing:
-    w, data = scipy.io.wavfile.read(file)
+    w, signal = readwav(file)
 
-    signal = np.zeros(data.shape[0])
-    if len(data.shape) == 2:
-        for i in range(data.shape[0]):
-            tmp1 = data[i][0]/2
-            tmp2 = data[i][1]/2
-            tmp = tmp1 + tmp2
-            signal[i] = tmp
-    else:
-        signal = data
-
-    T = 1
+    window_size = 256
     n = len(signal)
-    t = linspace(0, T, n, endpoint=False)
 
-    signal1 = rfft(signal)
-    signal1 = abs(signal1)
+    #print 'w: ', w, 'n: ', n
 
-    r1 = int(math.floor(60*n/w))
-    for i in range(1, r1):
-        signal1[i] = 0
+    f_list = []
 
-    r2 = int(math.ceil(300*n/w))
-    for i in range(r2, len(signal1)):
-        signal1[i] = 0
+    window = blackman(window_size)
+    num_of_wind = math.ceil(n/window_size)
+    print num_of_wind
 
-    m = max(signal1[1:])
+    for i in range(1, num_of_wind + 1):
+        chunk =
+        signal1 = rfft(signal)
+        signal1 = abs(signal1)
 
-    for i in range(r1 - 1, r2):
-        if abs(signal1[i]) < m/2:
+        r1 = int(math.floor(60*n/w))
+        for i in range(1, r1):
             signal1[i] = 0
 
-    r1 = int(math.floor(85*n/w))
-    r2 = int(math.ceil(180*n/w))
+        r2 = int(math.ceil(300*n/w))
+        for i in range(r2, len(signal1)):
+            signal1[i] = 0
 
-    men = 0
-    for i in range(r1, r2):
-        men += abs(signal1[i])
+        m = max(signal1[1:])
 
-    r1 = int(math.floor(165*n/w))
-    r2 = int(math.ceil(255*n/w))
+        for i in range(r1 - 1, r2):
+            if abs(signal1[i]) < m/2:
+                signal1[i] = 0
 
-    woman = 0
-    for i in range(r1, r2):
-        woman += abs(signal1[i])
 
-    for i in range(0, len(signal1)):
-        if signal1[i] > 0:
-            signal1[i] = log(signal1[i])
+        signal2 = irfft(signal1)
+        signal2 = abs(signal2)
 
-    signal2 = irfft(signal1)
-    signal2 = abs(signal2)
+        poz = 0
+        r1 = int(w/80)
+        r2 = int(w/255)
+        m = max(signal2[r2:r1])
+        for i in range(r2, r1):
+            if m == signal2[i]:
+                poz = i
 
-    poz = 0
-    r1 = int(w/80)
-    r2 = int(w/250)
-    m = max(signal2[r2:r1])
-    for i in range(r2, r1):
-        if m == signal2[i]:
-            poz = i
+        f_list.append(w/poz)
 
-    f = w/poz
+    f = median(array(f_list))
 
     print "Pitch: ", f
 
-    if f <= 165:
+    if f <= 175:
         print file, 'M'
     else:
-        if f >= 180:
-            print file, 'K'
-        else:
-            if men >= woman:
-                print file, 'M'
-            else:
-                print file, 'K'
+        print file, 'K'
